c14bb2b061feb8eb91b904012b2ddde76cae5a0d
Your Name <you@example.com>
2017-04-09 20:10:51 -0500

    intento sin map,filter y forEach por que no estan coperando XD

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -1,76 +1,99 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-    if (creditCardNumber == undefined){
+    if (creditCardNumber === undefined){
       return "Ingresa un numero";
     }
-
     if (typeof creditCardNumber  != 'number'){
       return "Error de dato";
     }
-
-    if (creditCardNumber.toString().length < 16){
-      return "Faltan numeros"
+    if (creditCardNumber.toString().length < 17){
+      return "Faltan numeros";
     }
 
+    var resultado = validacion(creditCardNumber);
+}
 
-    var stringTarjeta = creditCardNumber.toString();
+function validacion(creditCardNumber){
+  var stringTarjeta = creditCardNumber.toString();
+  console.log(stringTarjeta);
 
-    console.log(tarjeta,stringTarjeta);
+  var numeros= stringTarjeta.split("");
+  var par =[];
 
-    var numeros =stringTarjeta.split("");
-    var par = numeros.filter(function (numero) {
-        var posicion =(numeros.indexOf(numero));
-        if (posicion%2 == 0){
-          return parseInt(numero);
+  
+
+
+}
+function primerInento(creditCardNumber){
+  var stringTarjeta = creditCardNumber.toString();
+
+  console.log(tarjeta,stringTarjeta);
+
+  var numeros =stringTarjeta.split("");
+  var par = numeros.filter(function (numero) {
+      var posicion =(numeros.indexOf(numero));
+      if (posicion%2 == 0){
+        return parseInt(numero);
+      };
+    });
+
+    console.log(par)
+  var impar = numeros.filter(function (numero) {
+      var posicion =(numeros.indexOf(numero));
+      if (posicion%2 != 0){
+        return parseInt(numero);
         };
       });
 
-      console.log(par)
-    var impar = numeros.filter(function (numero) {
-        var posicion =(numeros.indexOf(numero));
-        if (posicion%2 != 0){
-          return parseInt(numero);
-          };
-        });
-
-        console.log(impar)
-    var primeroPar = par.map(function(num){
-        var unidades;
-        var decenas;
-        var numDoble =parseInt(num)*2
-        decenas = Math.floor(numDoble/10);
-        unidades = (numDoble %10);
-        var newPar = (unidades + decenas);
-        return newPar;
-      })
-
-    console.log(primeroPar);
-
-    var sumaImpar = impar.reduce(function(total,numero){
-      return parseInt(total) +parseInt(numero);
+      console.log(impar)
+  var primeroPar = par.map(function(num){
+      var unidades;
+      var decenas;
+      var numDoble =parseInt(num)*2
+      decenas = Math.floor(numDoble/10);
+      unidades = (numDoble %10);
+      var newPar = (unidades + decenas);
+      return newPar;
     })
 
-    console.log(sumaImpar)
-    var sumaPar = primeroPar.reduce(function(total,numero){
-      return parseInt(total) + parseInt(numero);
-    })
-    console.log(sumaImpar)
+  console.log(primeroPar);
 
+  var sumaImpar = impar.reduce(function(total,numero){
+    return parseInt(total) +parseInt(numero);
+  })
 
+  console.log(sumaImpar)
+  var sumaPar = primeroPar.reduce(function(total,numero){
+    return parseInt(total) + parseInt(numero);
+  })
+  console.log(sumaImpar)
 
 }
 
-function validateCreditCard(creditCardNumber){
-  var cardNumber = creditCardNumber;
-  console.log(cardNumber);
 
-}
 
+/*
 function isANumber(creditCardNumber){
   var isNumber = creditCardNumber;
-
+  if (isNumber === undefined){
+    return "Ingresa un numero";
+  }
 
 }
+
+function isNothing(creditCardNumber){
+  var cardNumber = creditCardNumber;
+  if (typeof cardNumber  != 'number'){
+    return "Error de dato";
+  }
+}
+
+function correctLength(creditCardNumber){
+  var isCardLength = creditCardNumber;
+  if (isCardLength.toString().length < 17){
+    return "Faltan numeros";
+  }
+}*/
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
